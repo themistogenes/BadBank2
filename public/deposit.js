3b8877556bfac4fe1cf4b6eb32818e908b90dfda
themistogenes <85630327+themistogenes@users.noreply.github.com>
2022-03-15 23:16:12 -0400

    Update deposit.js

--- public/deposit.js
+++ public/deposit.js
@@ -63,7 +63,8 @@ function DepositForm(props){
           //props.setStatus(JSON.stringify(data.value));
           props.setStatus('Deposit successful!  Hopefully that\'s money you won\'t miss...');
           console.log('Deposit JSON data:', data);
-          sictx.balance = data.value.balance;
+          //sictx.balance = data.value.balance;
+          sictx.balance = data.balance;
           console.log('Deposit sictx.balance: ' + sictx.balance);
           props.setShow(false);
         } catch(err) {
