e08e2e56a2b9e211475511abbcab4ec09ed5c279
themistogenes <85630327+themistogenes@users.noreply.github.com>
2022-03-15 21:26:37 -0400

    dotenv

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app     = express();
 var cors    = require('cors');
 var dal     = require('./dal.js');
+require ('dotenv').config();
 
 // used to serve static files from public directory
 app.use(express.static('public'));
