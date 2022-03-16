462dcdcb595b890bcda78607e68e6938fb69209f
themistogenes <85630327+themistogenes@users.noreply.github.com>
2022-03-15 22:55:02 -0400

    Update dal.js

--- dal.js
+++ dal.js
@@ -1,5 +1,4 @@
-require ('dotenv').config();
-//MONGODB_URI="mongodb+srv://badbankuser:badbankpass@Cluster0.n9z04.mongodb.net/sample_mflix?retryWrites=true&w=majority"
+//require ('dotenv').config();
 
 const MongoClient = require('mongodb').MongoClient;
 const url         = process.env.MONGODB_URI || 'mongodb://localhost:27017';
